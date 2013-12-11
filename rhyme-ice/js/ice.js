aa7b01754258de78f615d733ed678599485ca169
hotman.xx <hotman.xx@gmail.com>
2013-12-11 14:27:46 +0000

    基本完善了view与controller分离机制。初步做了字的音韵检测功能。

--- rhyme-ice/js/ice.js
+++ rhyme-ice/js/ice.js
@@ -20,26 +20,16 @@ if(typeof(com.hm_x.ice.Controller) == "undefined" || !com.hm_x.ice.Controller)
 		cpCatalog.init("conf/cp.xml");
 
 		var taxonomy = cpCatalog.getTaxonomy();
-		var kindSelector = $("ci-tag-kind-selector");
+		var kindSelector = this.widget.cpWidget.cpKindView;
 
 		taxonomy.each(function(tax){
-			var optGrp = document.createElement("optgroup");
-			optGrp.setAttribute("label", tax);
-			kindSelector.appendChild(optGrp);
-			
+			kindSelector.addGroup(tax);
 			var kindList = cpCatalog.getKindList(tax);
 			kindList.each(function(kind) {
-				var kindOpt = document.createElement("option");
-				kindOpt.setAttribute("value", kind);
-				kindOpt.appendChild(document.createTextNode(kind));
-				kindSelector.appendChild(kindOpt);
+				kindSelector.addOption(kind);
 			});
 		});
 
-		Event.observe("ci-tag-kind-selector", "change", this.onShowCiTagList.bindAsEventListener(this, kindSelector));
-		Event.observe("cp-name", "click", this.onSelectCiTag.bindAsEventListener(this));
-		$("cp-select-dialog").hide();
-		
 		com.hm_x.ice.Editor.init();
 	},
 	
@@ -48,35 +38,13 @@ if(typeof(com.hm_x.ice.Controller) == "undefined" || !com.hm_x.ice.Controller)
 		com.hm_x.ice.Editor.init(this.currentCiTag);
 	},
 	
-	onSelectCiTag : function(evt) {
-		if($("cp-select-dialog").visible())
-			$("cp-select-dialog").hide();
-		else
-			$("cp-select-dialog").show();
-	},
-	
-	onShowCiTagList : function(evt, kindSelector) {
-		var kind = kindSelector.value;
+	onShowCpList : function(kind) {
 		var ciTagList = this.cpCatalog.getCiTagList(kind);
-		var tagSelector = $("ci-tag-list");
-		com.hm_x.xml.clearChildren(tagSelector);
-		ciTagList.each(function(ciTag, i){
-			var ciTagName = ciTag.getAttribute("名");
-			var tagNode = document.createElement("span");
-			tagNode.setAttribute("ice-source", ciTag.getAttribute("源"));
-			tagNode.appendChild(document.createTextNode(ciTagName));
-			tagNode = $(tagSelector.appendChild(tagNode));
-			tagNode.addClassName("ci-tag-name");
-			tagNode.addClassName("selectable-item");
+		var widget = this.widget.cpWidget.cpListWidget;
+		widget.clear();
+		ciTagList.each(function(ciTag){
+			widget.addCp(ciTag);
 		});
-
-		Event.observe(tagSelector, "click", this.onClickCiTagDialog.bindAsEventListener(this));
-	},
-	
-	onClickCiTagDialog : function(evt) {
-		var ele = evt.element();
-		this.loadCiTag(ele.getAttribute("ice-source"));
-		$("cp-select-dialog").hide();
 	},
 	
 	onSelectRhymeKind : function(kindId) {
@@ -98,5 +66,12 @@ if(typeof(com.hm_x.ice.Controller) == "undefined" || !com.hm_x.ice.Controller)
 		this.rhyme.getDeptList().each(function(dept){
 			deptWidget.addDept(dept);
 		});
+	},
+	
+	checkMetrics : function() {
+		var peom = this.widget.editorView.htmlNode.value;
+		if(peom) {
+			var ri = this.rhyme.checkZi(peom[0]);
+		}
 	},
 };
