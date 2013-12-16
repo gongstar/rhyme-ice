2df476866480a3203c636df5360e7add9bd45893
hotman.xx <hotman.xx@gmail.com>
2013-12-16 14:14:22 +0000

    为数组增加 groupBy 方法

--- rhyme-ice/js/common.js
+++ rhyme-ice/js/common.js
@@ -17,4 +17,20 @@ if(!com.hm_x.common)
 	
 	this.isChrome	= agent.indexOf("Chrome") != -1;
 	this.isFirefox	= agent.indexOf("Firefox") != -1;
+	
+	// 全局工具方法
+	/**
+		@args sepFunc 是一个函数，它将依次被调用，得到一个元素作为参数。应该返回一个索引值用于分组。
+	*/
+	Array.prototype.groupBy = function(sepFunc) {
+		var groups = [];
+		this.each(function(it){
+			var idx = sepFunc(it);
+			if(!(idx in groups))
+				groups[idx] = [it];
+			else
+				groups[idx].push(it);
+		});
+		return groups;
+	}
 })();
